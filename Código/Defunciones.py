16da30ac19d34bdb343c0c430d30be2ea1649df3
jonnvega <47796239+jonnvega@users.noreply.github.com>
2019-12-18 14:04:52 -0600

    18 Dic 19

--- Código/Defunciones.py
+++ Código/Defunciones.py
@@ -4,71 +4,93 @@ import seaborn as sns
 import numpy as np
  
 pd.set_option('display.max_columns', None)
+matplotlib.rcParams['font.family'] = "monospace"
 
-defu_raw = pd.read_excel("C:\\Users\\jonva\\Downloads\\DEFU2016.xlsx")
+#http://sistemas.inec.cr/pad4/index.php/catalog/166/datafile/F1
+#diccionario: http://sistemas.inec.cr/pad4/index.php/catalog/166/datafile/F1
+#python visualization gallery: https://python-graph-gallery.com/ 
+
+defu_raw = pd.read_excel("C:\\Users\\jonva\\GitHub\\PyDa_Nov2019\\DEFU2016.xlsx")
 
 defu = defu_raw
+#defu.shape
 # defu.head()
-# print(defu.info())
-# defu_var = pd.read_excel("C:\\Users\\jonva\\Downloads\\DEFU2016.xlsx", sheet_name='ListVariables')
-# defu_var = defu_var.drop('Variable', axis=1).drop(defu_var.index[0:4])
-# defu_var.columns
-
+# defu.tail()
+# defu.info()
 
 defu = defu.drop(defu.index[0]).drop('Variable', axis=1)
 
 # defu.columns
+# defu.dtypes
 
-# plt.hist(defu['edads'], bins=30)
+# defu[(defu['instmurio']==3) & (defu['g62']==2)]
+
+# df[(df['col1'] >= 3) & (df['col1'] <=1 )]
+
+# defu['edads'].plot.hist(bins=30, title='Distribución de Defunciones por Edad')
+
 # sns.boxplot(x=defu['edads'])
 
-defu['edads'].plot.hist(bins=30, title='Distribución de Defunciones por Edad')
-defu.hist(column = 'edads', bins=30, grid=False)
-sns.distplot(defu['edads'], kde=False, bins=30)
+# defu.groupby('edads').count().iloc[:,0]
+
+# defu.hist(column = 'edads', bins=30, grid=False)
+# sns.distplot(defu['edads'], kde=False, bins=30)
 
 defu = defu[defu['edads']<150]
 
 # defu.loc[:,['diadef','mesdef','anodef']].head()
 
-defu['fecha_def'] = (defu['anodef']*10000 + defu['mesdef']*100+defu['diadef']).astype(int)
+defu['fecha_def'] = (defu['anodef']*10000 + defu['mesdef']*100+defu['diadef'])
 
 # defu['fecha_def'].head()
 
 defu['fecha_def'] = pd.to_datetime(defu['fecha_def'], format ='%Y%m%d')
 
-defu = defu[defu['anodef'].astype(int)>2013]
+defu = defu[defu['anodef'].astype(int)==2016]
 
-# defu['anodef'].value_counts()
 
-# defu['fecha_def'].plot.bar()
+# defu.groupby([defu['fecha_def'].dt.year, defu['fecha_def'].dt.month]).count()['anotrab'].plot(kind='bar')
+# defu.groupby(defu['fecha_def'].dt.month).count()['anotrab'].plot(kind='bar')
+
 
 sexo_iu = defu.groupby(['sexo', 'iu'])
-sexo_iu['edads'].count()
+# sexo_iu['edads'].count()
+# sexo_iu['edads'].count().reset_index()
 
-ax = sns.boxplot(x=defu['sexo'], y=defu['edads'])
-ax.set(xlabel='Sexo', ylabel='Edad')
-
+# ax = sns.boxplot(x=defu['sexo'], y=defu['edads'],)
+# ax.set(xlabel='Sexo', ylabel='Edad')
 
 defu['provincia_tex'] = defu['provincia'].replace({1:'San Jose',2:'Alajuela', 3:'Cartago', 4:'Heredia', 5:'Guanacaste', 6:'Puntarenas', 7:'Limón'})
 
-plt.figure(figsize=(20,10)) 
-plt.xlabel('xlabel', fontsize=30)
-plt.ylabel('ylabel', fontsize=16)
-ax = sns.boxplot(data=defu, x='provincia_tex', y='edads', hue='sexo', )
-legend = plt.legend(title = 'Sexo', loc='upper left', bbox_to_anchor=(1, 1), prop={'size':20})
-plt.setp(legend.get_title(),fontsize='20')
-ax.set(xlabel='Provincia', ylabel='Edad')
-ax.set_title("Defunciones por Edad, Provincia y Sexo", fontsize='40')
-ax.tick_params(axis = 'both', which = 'major', labelsize = 20)
+# defu['provincia_tex'].value_counts()
+# defu['provincia_tex'].value_counts(normalize=True)
 
-defu['causamuer'].value_counts().head(20).plot.bar()
+
+plt.figure(figsize=(10,5)) 
+plt.xlabel('xlabel', fontsize=30)
+plt.ylabel('ylabel', fontsize=30)
+ax = sns.boxplot(data=defu, x='provincia_tex', y='edads', hue='sexo', )
+plt.legend(title = 'Sexo', bbox_to_anchor=(1, 1), prop={'size':10})
+plt.xlabel('Provincia', fontsize=18)
+plt.ylabel('Edad', fontsize=16)
+ax.set_title("Defunciones por Edad, Provincia y Sexo", fontsize='20')
+ax.tick_params(axis = 'both', labelsize = 12)
+
+
+
+fig, ax = plt.subplots(figsize=(15, 15))
+ax = sns.violinplot(x="provincia_tex", y="edads",  data=defu, cut = 0)
+ax.set_xlabel("Sexo",size = 20,alpha=0.8)
+ax.set_ylabel("Edad",size = 20,alpha=0.8)
+ax.set_title("Defunciones por Edad y Sexo",size=20)
+ax.tick_params(axis = 'both', labelsize = 20)
 
 
 
 ##Categoría de defunción
-sns.catplot(y='g18', kind='count', palette="ch:.25", data=defu)
+# defu['g17'].value_counts().head(20).plot.bar()
 
-g17 = pd.read_csv("C:\\Users\\jonva\\Desktop\\PyDa\\g17.csv",encoding='latin-1')
+g17 = pd.read_csv("C:\\Users\\jonva\\GitHub\\PyDa_Nov2019\\Datos\\g17.csv",encoding='latin-1')
 g17['g17'].str[7:]
 
 def remove_char(oldstring, oldchar, newchar):
@@ -85,12 +107,26 @@ for i in list(g17['g17']):
     cat = cat+1
 defu['g18'].value_counts()
 
-sns.catplot(y='g18', kind='count', palette="ch:.25", data=defu)
+g18_count = defu.groupby('g18').count().reset_index()
 
-causa = defu.groupby(['g17','sexo'])
-causa['anotrab'].sum()
+g18_count.sort_values(by='edads',ascending=False)[['g18','edads']]
+
+ax = sns.barplot(x='edads', y='g18', palette="ch:.25", data=g18_count)
+ax.set(xlabel='Edad', ylabel='Causa de Defunción', title='Defunciones por edad según causa de defunción')
+
+
+defu.groupby(['g18', 'provincia_tex']).count().reset_index().pivot(index='provincia_tex', columns='g18', values='anotrab')
+
+ax = sns.heatmap(defu.groupby(['g18', 'provincia_tex']).count().reset_index().pivot(index='provincia_tex', columns='g18', values='anotrab'))
 
 ##nombres son muy largos
 defu.groupby('g18')['anotrab'].count()
 
-##cortando los nombres
+
+g18_instmurio = defu[defu['provincia_tex']=='Limón'].groupby(['g18', 'instmurio'])['anotrab'].count().reset_index()
+
+g18_instmurio = g18_instmurio[g18_instmurio['anotrab']>10]
+
+g18_instmurio.pivot(columns='g18', index='instmurio').plot(kind='bar', stacked=True)
+plt.legend(title = 'Sexo', bbox_to_anchor=(1, 1), prop={'size':10})
+
--- Código/Estaciones.py
+++ Código/Estaciones.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import time
 
 result = requests.get("https://www.imn.ac.cr/estaciones-automaticas")
 
@@ -31,7 +32,6 @@ nombres = [t.text for t in td if t.attrs['width']=='244']
 enlaces = soup.find_all("a")
 
 # print(enlaces)
-# print("\n")
 
 estaciones = [enlace.attrs['href'] for enlace in enlaces if "Entrar" in enlace.text]
 
@@ -47,6 +47,12 @@ estaciones = [enlace.attrs['href'] for enlace in enlaces if "Entrar" in enlace.t
 
 # tabla_lib = 'https://www.imn.ac.cr'+iframe_lib['src']
 
+# resultado_tabla_lib = requests.get(tabla_lib)
+# fuente_tabla_lib = resultado_tabla_lib.content
+# soup_tabla_lib = BeautifulSoup(fuente_tabla_lib, 'lxml')
+# tabla_html_lib = soup_tabla_lib.select('table')[0]
+# tabla_lib = pd.read_html(str(tabla_html_lib))[0]
+
 def obtener_tabla(enlace):
     
     resultado = requests.get(enlace)
@@ -55,7 +61,6 @@ def obtener_tabla(enlace):
     iframe = soup.select('iframe')[0]
     url_tabla = 'https://www.imn.ac.cr'+iframe['src']
     
-
     resultado_tabla = requests.get(url_tabla)
     fuente_tabla = resultado_tabla.content
     soup_tabla = BeautifulSoup(fuente_tabla, 'lxml')
@@ -68,18 +73,34 @@ def obtener_tabla(enlace):
     return tabla
 
 
+
+
 # obtener_tabla(estaciones[2]).iloc[0]
 
 # tablas = list(map(obtener_tabla, estaciones))
 
-# for enlace in estaciones:
-#     resultado = requests.get(enlace)
-#     fuente = resultado.content
-#     soup = BeautifulSoup(fuente, 'lxml')
-#     if not soup.select('iframe'):
-#         print(estaciones.index(enlace))
+for enlace in estaciones:
+    resultado = requests.get(enlace)
+    fuente = resultado.content
+    soup = BeautifulSoup(fuente, 'lxml')
+    iframe = soup.select('iframe')[0]
+    url_tabla = 'https://www.imn.ac.cr'+iframe['src']
+    resultado_tabla = requests.get(url_tabla)
+    fuente_tabla = resultado_tabla.content
+    soup_tabla = BeautifulSoup(fuente_tabla, 'lxml')
+    if not soup_tabla.select('table'):
+        print(estaciones.index(enlace),"\n")
 
-del estaciones[31], cantones[31]
+del estaciones[6], cantones[6], nombres[6]
+
+for enlace in estaciones:
+    resultado = requests.get(enlace)
+    fuente = resultado.content
+    soup = BeautifulSoup(fuente, 'lxml')
+    if not soup.select('iframe'):
+        print(estaciones.index(enlace),"\n")
+
+del estaciones[29], cantones[29], nombres[29]
 
 tablas = list(map(obtener_tabla, estaciones))
 
@@ -88,27 +109,71 @@ temp_actual = pd.DataFrame(columns=['Estacion', 'Canton','Fecha','Temp', 'Lluvia
 for tabla in tablas:
     temp_actual = pd.concat([temp_actual,tabla.loc[[0],['Estacion','Canton','Fecha','Temp', 'Lluvia']]],ignore_index=True)
 
+temp_actual.head()
+temp_actual.shape
+
 # temp_actual.isna().sum()
 temp_actual.dropna(axis='index', how='any', inplace=True)
-
+temp_actual = temp_actual.reset_index()
+temp_actual.drop('index', axis=1, inplace=True)
 # temp_actual.dtypes
 
 temp_actual['Temp'] = temp_actual['Temp'].astype(float)
 
 temp_actual['Temp'] = temp_actual['Temp'].apply(lambda x : x/100 if x>100 else x)    
 
-# temp_actual.head()
-
 
 from selenium import webdriver
 
-browser = webdriver.Chrome("C:\\Users\\jonva\\Downloads\\chromedriver")
+print(temp_actual['Estacion'])
 
+Cantones = pd.Series(temp_actual['Canton'].unique())
+
+
+lat_list = []
+lon_list = []
+
+browser = webdriver.Chrome("C:\\Users\\jonva\\Downloads\\chromedriver")
 browser.get('https://www.coordenadas-gps.com/mapa/pais/CR')
-dirrecion = browser.find_element_by_id('address')
-dirrecion.send_keys(temp_actual[1,'Estacion'])
-browser.find_element_by_class_name('btn').click()
-latitud = browser.find_element_by_id('latitude')
-latitud.get_attribute('value')
-longitud = browser.find_element_by_id('longitude')
-longitud.get_attribute('value')
+
+for canton in Cantones:
+    try:
+        direccion = browser.find_element_by_id('address')
+        direccion.send_keys(canton)
+        browser.find_element_by_class_name('btn').click()
+        time.sleep(5)
+        latitud = browser.find_element_by_id('latitude')
+        lat_value = str(latitud.get_attribute('value'))
+        longitud = browser.find_element_by_id('longitude')
+        lon_value = str(longitud.get_attribute('value'))
+        lat_list.append(lat_value)
+        lon_list.append(lon_value)
+    except:
+        lat_value = np.nan
+        lon_value = np.nan
+        lat_list.append(lat_value)
+        lon_list.append(lon_value)
+    browser.find_element_by_id('address').clear()
+
+
+len(lat_list)==len(lon_list)==len(Cantones)
+
+cantones_coor = pd.DataFrame(columns=['Canton', 'Latitud', 'Longitud'],index=None)
+cantones_coor['Canton'] = Cantones
+cantones_coor['Latitud'] = lat_list
+cantones_coor['Longitud'] = lon_list
+
+cantones_coor.head()
+cantones_coor[cantones_coor['Latitud'].isnull()].index.tolist()
+
+cantones_coor.loc[17,'Latitud'] = 10.103415
+cantones_coor.loc[17,'Longitud'] = -85.4153082
+
+
+from bokeh.plotting import figure, show, output_notebook
+from bokeh.tile_providers import CARTODBPOSITRON
+p = figure(x_range=(-9780000, -9745000), y_range=(5130000, 5160000),
+           x_axis_type="mercator", y_axis_type="mercator")
+p.add_tile(CARTODBPOSITRON)
+output_notebook()
+show(p)
